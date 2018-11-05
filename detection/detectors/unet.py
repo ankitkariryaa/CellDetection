85f5d83fe254f48cac853531462c200aeafbc411
Ankit <ankit.ky@gmail.com>
2018-11-05 16:33:22 +0000

    Update unet

--- detection/detectors/unet.py
+++ detection/detectors/unet.py
@@ -19,8 +19,8 @@ class UNet(FCNDetector):
         super().__init__(input_shape, learning_rate, no_classes, weight_file)
 
     def build_model(self):
-        input = Input(batch_shape=self.input_shape, name='input_1')
-        conv1 = Conv2D(64, 3, activation='relu', padding='same')(input)
+        inputs = Input(batch_shape=self.input_shape, name='input_1')
+        conv1 = Conv2D(64, 3, activation='relu', padding='same')(inputs)
         conv1 = Conv2D(64, 3, activation='relu', padding='same')(conv1)
         pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
         conv2 = Conv2D(128, 3, activation='relu', padding='same')(pool1)
@@ -40,34 +40,34 @@ class UNet(FCNDetector):
 
         up6 = Conv2D(512, 2, activation='relu', padding='same')(
             UpSampling2D(size=(2, 2))(drop5))
-        merge6 = Concatenate([drop4, up6], axis=3)
+        merge6 = concatenate([drop4, up6], axis=3)
         conv6 = Conv2D(512, 3, activation='relu', padding='same')(merge6)
         conv6 = Conv2D(512, 3, activation='relu', padding='same')(conv6)
 
         up7 = Conv2D(256, 2, activation='relu', padding='same')(
             UpSampling2D(size=(2, 2))(conv6))
-        merge7 = Concatenate([conv3, up7], axis=3)
+        merge7 = concatenate([conv3, up7], axis=3)
         conv7 = Conv2D(256, 3, activation='relu', padding='same')(merge7)
         conv7 = Conv2D(256, 3, activation='relu', padding='same')(conv7)
 
         up8 = Conv2D(128, 2, activation='relu', padding='same')(
             UpSampling2D(size=(2, 2))(conv7))
-        merge8 = Concatenate([conv2, up8], axis=3)
+        merge8 = concatenate([conv2, up8], axis=3)
         conv8 = Conv2D(128, 3, activation='relu', padding='same')(merge8)
         conv8 = Conv2D(128, 3, activation='relu', padding='same')(conv8)
 
         up9 = Conv2D(64, 2, activation='relu', padding='same')(
             UpSampling2D(size=(2, 2))(conv8))
-        merge9 = Concatenate([conv1, up9], axis=3)
+        merge9 = concatenate([conv1, up9], axis=3)
         conv9 = Conv2D(64, 3, activation='relu', padding='same')(merge9)
         conv9 = Conv2D(64, 3, activation='relu', padding='same')(conv9)
         conv9 = Conv2D(2, 3, activation='relu', padding='same')(conv9)
         conv10 = Conv2D(1, 1, activation='sigmoid')(conv9)
 
-        model = Model(input=input, output=conv10)
+        model = Model(outputs=conv10, inputs=inputs)
         optimizer = Adam(lr=self.learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
         model.compile(optimizer=optimizer,
-                      loss={'class_out': 'binary_crossentropy'}, metrics=['binary_accuracy'])
+                      loss='binary_crossentropy', metrics=['accuracy'])
         if self.weight_file:
             model.load_weights(self.weight_file)
         model.summary()
